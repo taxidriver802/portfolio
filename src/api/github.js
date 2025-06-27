03af828859ae8269f440704f83f1120c8f455d87
jason cox <JAcox12@icloud.com>
2025-06-27 05:04:18 -0500

    changed to tokenless fetch request to be able to push to gh-pages

--- src/api/github.js
+++ src/api/github.js
@@ -1,12 +1,8 @@
 export const fetchRepos = async (username) => {
-  const token = import.meta.env.VITE_GITHUB_TOKEN;
+  const res = await fetch(`https://api.github.com/users/${username}/repos`);
 
-  const res = await fetch(`https://api.github.com/users/${username}/repos`, {
-    headers: {
-      Authorization: `token ${token}`,
-    },
-  });
-  if (!res.ok) throw new Error(`Failed to fetch Github repos`);
+  if (!res.ok) throw new Error(`Failed to fetch GitHub repos`);
+
   const data = await res.json();
   return data;
 };
